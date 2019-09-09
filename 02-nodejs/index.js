39460de9c2dab2a0a0787f5e9b13ce28d8205f0e
patryckx <eng.patrickmonteiro@gmail.com>
2019-09-09 15:54:30 -0300

    resolve with promises

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -3,63 +3,100 @@
  1 Obter o numero de telefone de um usuario a partir de seu ID
  2 Obter o endereco do usuario pelo Id
 */
+const util = require('util')
+const obterEnderecoAsync = util.promisify(obterEndereco)
 
-function obterUsuario(callback) {
-  setTimeout(function () {
-    return callback (null, {
-      id: 1,
-      nome: 'Aladin',
-      dataNascimento: new Date()
-    })
-  }, 1000)
+function obterUsuario() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      return resolve({
+        id: 1,
+        nome: 'Aladin',
+        dataNascimento: new Date()
+      })
+    }, 1000)
+  })
 }
 
-function obterTelefone(idUsuario, callback) {
-  setTimeout(() => {
-    return  callback(null, {
-      telefone: '1231321',
-      ddd: 11
-    })
-  }, 2000)
+function obterTelefone(idUsuario) {
+  return new Promise(function resolverPromise(resolve, reject) {
+    setTimeout(() => {
+      return  resolve({
+        telefone: '1231321',
+        ddd: 11
+      })
+    }, 2000)
+  })
 }
 
 function obterEndereco(idUsuario, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      rua: 'Tv barão',
-      numero: '007'
-    })
-  }, 2000)
+  // return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return callback(null, {
+        rua: 'Tv barão',
+        numero: '007'
+      })
+    }, 2000)
+  // })
 }
 
-function resolverUsuario(erro, usuario) {
-  console.log('usuario', usuario)
-}
-
-obterUsuario(function resolverUsuario(error, usuario) {
-  if(error) {
-    console.error('Deu ruim no USUARIO', error)
-    return;
-  }
-  obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
-    if(error1) {
-      console.error('Deu ruim no TELEFONE', error)
-      return;
-    }
-    obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
-      if(error2) {
-        console.error('Deu ruim no ENDEREÇO', error)
-        return;
+const usuarioPromise = obterUsuario()
+// usuario -> telefone ->
+usuarioPromise
+  .then(function (usuario) {
+    return obterTelefone(usuario.id)
+      .then(function resolverTelefone(result) {
+        return {
+          usuario: {
+            nome: usuario.nome,
+            id:usuario.id
+          },
+          telefone: result
+        }
+      })
+  })
+  .then(function (resultado) {
+    const endereco = obterEnderecoAsync(resultado.usuario.id)
+    return endereco.then(function resolverEndereco(result) {
+      return {
+        usuario: resultado.usuario,
+        telefone: resultado.telefone,
+        endereco: result
       }
-
-      console.log(`
-        Nome: ${usuario.nome},
-        Endereco: ${endereco.rua},${endereco.numero},
-        Telefone: (${telefone.ddd}) ${telefone.telefone}
-      `)
     })
   })
-})
+  .then((resultado) => {
+    console.log(`
+      Nome: ${resultado.usuario.nome}
+      Endereco: ${resultado.endereco.rua}, ${resultado.endereco.numero}
+      Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+    `)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 
-// const telefone = obterTelefone(usuario.id)
-// console.log('telefone', telefone)
+// obterUsuario(function resolverUsuario(error, usuario) {
+//   if(error) {
+//     console.error('Deu ruim no USUARIO', error)
+//     return;
+//   }
+//   obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+//     if(error1) {
+//       console.error('Deu ruim no TELEFONE', error)
+//       return;
+//     }
+//     obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+//       if(error2) {
+//         console.error('Deu ruim no ENDEREÇO', error)
+//         return;
+//       }
+
+//       console.log(`
+//         Nome: ${usuario.nome},
+//         Endereco: ${endereco.rua},${endereco.numero},
+//         Telefone: (${telefone.ddd}) ${telefone.telefone}
+//       `)
+//     })
+//   })
+// })
