279e40ea739eea9966b3a4b69670ca316799e6c1
patryckx <eng.patrickmonteiro@gmail.com>
2019-09-11 16:12:44 -0300

    tests mocha

---
+++ 06-cli/database.js
@@ -0,0 +1,42 @@
+const { readFile, writeFile } = require('fs')
+
+const { promisify } = require('util')
+
+const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
+
+class Database {
+  constructor() {
+    this.NOME_ARQUIVO = 'herois.json'
+  }
+
+  async obterDadosArquivo() {
+    const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
+    return JSON.parse(arquivo.toString())
+  }
+
+  async escreverArquivo(dados) {
+    await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+    return true
+  }
+
+  async cadastrar(heroi) {
+    const dados = await this.obterDadosArquivo()
+    const id = heroi.id <= 2 ? heroi.id: Date.now()
+    const heroiComId = {
+      id,
+      ...heroi
+    }
+    const dadosFinal = [...dados, heroiComId]
+    const resultado = await this.escreverArquivo(dadosFinal)
+    return resultado
+  }
+
+  async listar (id) {
+    const dados = await this.obterDadosArquivo()
+    const dadosFiltrados = dados.filter(item => (id ? (item.id === id ): true))
+    return dadosFiltrados
+  }
+}
+
+module.exports = new Database()
---
+++ 06-cli/test.js
@@ -0,0 +1,26 @@
+const {
+  deepEqual,
+  ok
+} = require('assert')
+
+const database = require('./database')
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: 'Flash',
+  poder: 'Speed',
+  id: 1
+}
+
+describe('Suite de manipulação de Herois', () => {
+  it('Deve pesquisar um heroi usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    const [resultado] = await database.listar(expected.id)
+    deepEqual(resultado, expected)
+  })
+  it('deve cadastrar um heroi, usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+
+    ok(actual, expected)
+  })
+})
